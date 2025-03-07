bb90a9f107421a8b90f97545d66ab55d20571a53
Enrique Angrisano <milangrisano@gmail.com>
2025-03-06 20:53:08 -0500

    handlers configuration

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -3,11 +3,14 @@ import 'package:responsive_app/router/router_handlers.dart';
 
 class Flurorouter {
   static final FluroRouter router = FluroRouter();
+  static String login = '/login';
+  static String landingPage = '/:page';
 
   static void configureRoutes(){
     
     // Rutas
-    router.define('/:page', handler: homeHandler );
+    router.define(landingPage, handler: homeHandler );
+    router.define(login, handler: loginHandler);
 
     // 404
     router.notFoundHandler = homeHandler;
